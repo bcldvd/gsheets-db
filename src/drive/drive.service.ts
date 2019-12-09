78b65f6690960762fd7548ad7e49f754fff04487
davbocle <david.bocle@misys.com>
2019-12-09 08:22:45 +0100

    fix build

--- src/drive/drive.service.ts
+++ src/drive/drive.service.ts
@@ -9,21 +9,26 @@ export class DriveService {
         private config: ConfigService,
     ) {}
 
-    async listAppData(accessToken?: string) {
-        const oauth2Client = new google.auth.OAuth2(
-            this.config.get('CLIENT_ID'),
-            this.config.get('CLIENT_SECRET'),
-        );
-        oauth2Client.credentials = {
-            access_token: accessToken,
-        };
-
-        const  drive = google.drive({version: 'v3', auth: oauth2Client});
+    async listAppData(accessToken: string) {
+        const  drive = google.drive({version: 'v3', auth: this.getConfigFromToken(accessToken)});
         const params = {
             spaces: 'appDataFolder',
             pageSize: 3,
         };
         const res = await drive.files.list(params);
         return res.data;
+    }
+
+    private getConfigFromToken(accessToken: string) {
+        const oauth2Client = new google.auth.OAuth2(
+            this.config.get('CLIENT_ID'),
+            this.config.get('CLIENT_SECRET'),
+        );
+
+        oauth2Client.credentials = {
+            access_token: accessToken,
+        };
+
+        return oauth2Client;
     }
 }

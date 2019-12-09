1d469ac959677739c8a75a0252fcc48265895b9a
davbocle <david.bocle@misys.com>
2019-12-09 09:20:04 +0100

    [drive] get app data config file

--- src/drive/drive.controller.ts
+++ src/drive/drive.controller.ts
@@ -12,6 +12,10 @@ export class DriveController {
   @Get('appdata')
   @UseGuards(AuthGuard('jwt'))
   async listAppData(@Req() req) {
+    /* await this.driveService.createAppDataConfigFile(req.user.accessToken, {
+      pathOfDb: './lafay-db.gsheets',
+    }); */
+    // return this.driveService.getAppDataConfigFile(req.user.accessToken);
     return await this.driveService.listAppData(req.user.accessToken);
   }
 }
--- src/drive/drive.service.ts
+++ src/drive/drive.service.ts
@@ -1,34 +1,95 @@
 import { Injectable } from '@nestjs/common';
-import {google} from 'googleapis';
+import { google } from 'googleapis';
 import { ConfigService } from '../config/config.service';
 
 @Injectable()
 export class DriveService {
+  configFile = 'config.json';
 
-    constructor(
-        private config: ConfigService,
-    ) {}
+  constructor(private config: ConfigService) {}
 
-    async listAppData(accessToken: string) {
-        const  drive = google.drive({version: 'v3', auth: this.getConfigFromToken(accessToken)});
-        const params = {
-            spaces: 'appDataFolder',
-            pageSize: 3,
-        };
-        const res = await drive.files.list(params);
-        return res.data;
-    }
+  async listAppData(accessToken: string) {
+    const drive = this.getDriveAPI(accessToken);
+    const params = {
+      spaces: 'appDataFolder',
+      pageSize: 3,
+    };
+    const res = await drive.files.list(params);
+    return res.data;
+  }
 
-    private getConfigFromToken(accessToken: string) {
-        const oauth2Client = new google.auth.OAuth2(
-            this.config.get('CLIENT_ID'),
-            this.config.get('CLIENT_SECRET'),
-        );
+  async createAppDataConfigFile(accessToken: string, content: ConfigFile) {
+    const options = {
+        accessToken,
+        fileName: this.configFile,
+        parents: ['appDataFolder'],
+        content,
+    };
+    return await this.createFile(options);
+  }
 
-        oauth2Client.credentials = {
-            access_token: accessToken,
-        };
+  async getAppDataConfigFile(accessToken: string): Promise<ConfigFile> {
+    const appDataFiles = await this.listAppData(accessToken);
+    const configFileInfo = appDataFiles.files.find(file => file.name === this.configFile);
+    const fileId = configFileInfo.id; // TODO: handle not found
+    return await this.getFile(accessToken, fileId) as Promise<ConfigFile>;
+  }
 
-        return oauth2Client;
-    }
+  async createFile(options: CreateFileOpts) {
+    const drive = this.getDriveAPI(options.accessToken);
+    const fileMetadata = {
+      name: options.fileName,
+      parents: options.parents,
+    };
+    const media = {
+      mimeType: 'application/json',
+      body: JSON.stringify(options.content),
+    };
+    const res = await drive.files.create({
+      requestBody: fileMetadata,
+      media,
+    });
+    return res.data;
+  }
+
+  async getFile(accessToken: string, fileId: string) {
+    const drive = this.getDriveAPI(accessToken);
+    const params = {
+        fileId,
+        alt: 'media',
+    };
+    const res = await drive.files.get(params);
+    return res.data;
+  }
+
+  private getDriveAPI(accessToken: string) {
+    return google.drive({
+      version: 'v3',
+      auth: this.getConfigFromToken(accessToken),
+    });
+  }
+
+  private getConfigFromToken(accessToken: string) {
+    const oauth2Client = new google.auth.OAuth2(
+      this.config.get('CLIENT_ID'),
+      this.config.get('CLIENT_SECRET'),
+    );
+
+    oauth2Client.credentials = {
+      access_token: accessToken,
+    };
+
+    return oauth2Client;
+  }
+}
+
+export interface CreateFileOpts {
+    accessToken: string;
+    fileName: string;
+    parents: string[];
+    content: string | any;
+}
+
+export interface ConfigFile {
+    pathOfDb: string;
 }

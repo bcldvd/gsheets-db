0eb7855e13dea1970153d4ad9fecb25796fe9784
David <david.bocle@finastra.com>
2020-02-06 08:38:44 +0100

    new get data

--- src/drive/sheets.service.spec.ts
+++ src/drive/sheets.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DriveService } from './drive.service';
 import { ConfigModule } from '../config/config.module';
 import { SheetsService } from './sheets.service';
+import { MOCK_DATA } from './sheets.mock';
 
 describe('SheetsService', () => {
   let service: SheetsService;
@@ -19,17 +20,83 @@ describe('SheetsService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('matrixToRowData', () => {
-    it('should convert matrix to row data', () => {
-      const mockData = [['day', 'workout', 'reps'], ['02/11/1991', 'C3', '12']];
-      const expected = [
+  describe('get data', () => {
+    it('should use split titles and data', () => {
+      const expectedTitles = [
+        'Date',
+        'Humeur',
+        'BPM',
+        'Répétitions',
+        '',
+        '',
+        '',
+        '',
+        '',
+        'Répétitions',
+      ];
+
+      const { titles, data } = service['splitValuesAndTitles'](
+        MOCK_DATA.sheetValues,
+      );
+
+      expect(titles).toEqual(expectedTitles);
+      expect(data[1][0]).toBe('06/01/2016');
+    });
+
+    it('should format merges accordingly', () => {
+      const expectedMerges = [
         {
-          day: '02/11/1991',
-          workout: 'C3',
-          reps: '12',
+          startRowIndex: 1,
+          endRowIndex: 10,
+          subGroups: [
+            {
+              startColumnIndex: 3,
+              endColumnIndex: 9,
+            },
+            {
+              startColumnIndex: 9,
+              endColumnIndex: 15,
+            },
+          ],
         },
       ];
-      expect(service['matrixToRowData'](mockData)).toStrictEqual(expected);
+
+      const formattedMerges = service['formatMerges'](MOCK_DATA.merges);
+
+      expect(formattedMerges[0]).toStrictEqual(expectedMerges[0]);
+      expect(formattedMerges[1]).not.toHaveProperty('endRowIndex');
+      // Being the last merge, there's no end row
+    });
+
+    it('should format data accordingly', () => {
+      const { titles, data } = service['splitValuesAndTitles'](
+        MOCK_DATA.sheetValues,
+      );
+      const merges = service['formatMerges'](MOCK_DATA.merges);
+      const expectedRows = [
+        {
+          Date: '06/01/2016',
+          Humeur: '💪🏼',
+          BPM: '',
+          groupTitle: 'Niveau 2',
+          subGroups: [
+            {
+              title: 'B1',
+              headerTitle: 'Répétitions',
+              values: ['6', '5', '4', '3', '2', '1'],
+            },
+            {
+              title: 'A3',
+              headerTitle: 'Répétitions',
+              values: ['6', '5', '4', '3', '2', '1'],
+            },
+          ],
+        },
+      ];
+
+      const rowData = service['toRowData'](data, merges, titles);
+
+      expect(rowData[0]).toMatchObject(expectedRows[0]);
     });
   });
 });
--- src/drive/sheets.service.ts
+++ src/drive/sheets.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { google, sheets_v4 } from 'googleapis';
 import { DriveService } from './drive.service';
 
+const DEFAULT_RANGE_ALL = 'A1:Z1000';
+
 @Injectable()
 export class SheetsService {
   constructor(private driveService: DriveService) {}
@@ -26,7 +28,103 @@ export class SheetsService {
       ranges,
     });
 
-    return this.rawSheetToMatrix(rawSheet.data);
+    return rawSheet;
+  }
+
+  async getData(
+    accessToken: string,
+    spreadsheetId: string,
+    range = DEFAULT_RANGE_ALL,
+  ) {
+    const ranges = [range];
+    const sheets = this.getSheetsAPI(accessToken);
+    const rawSheet = await sheets.spreadsheets.get({
+      spreadsheetId,
+      includeGridData: false,
+      ranges,
+    });
+    const sheetValues = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
+
+    const merges = this.formatMerges(rawSheet.data.sheets[0].merges);
+    const { titles, data } = this.splitValuesAndTitles(sheetValues.data.values);
+    return this.toRowData(data, merges, titles);
+  }
+
+  private splitValuesAndTitles(sheetValues: string[][]) {
+    const titles = sheetValues[0];
+    const data = sheetValues.slice(1, sheetValues.length);
+    return { titles, data };
+  }
+
+  private formatMerges(rawMerges: sheets_v4.Schema$GridRange[]) {
+    const mergesArray = [];
+    const merges = {};
+    rawMerges.forEach(merge => {
+      if (!merges.hasOwnProperty(merge.startRowIndex)) {
+        merges[merge.startRowIndex] = [];
+      }
+      merges[merge.startRowIndex].push(merge);
+    });
+    delete merges[0];
+
+    Object.keys(merges).forEach((key, index, array) => {
+      const subGroups = [
+        ...merges[key]
+          .map(({ startColumnIndex, endColumnIndex }) => ({
+            startColumnIndex,
+            endColumnIndex,
+          }))
+          .slice(1, merges[key].length),
+      ];
+      const merge = {
+        startRowIndex: merges[key][0].startRowIndex,
+        subGroups,
+      };
+      if (index > 0 && index < array.length) {
+        mergesArray[index - 1].endRowIndex = merges[key][0].startRowIndex;
+      }
+      mergesArray.push(merge);
+    });
+
+    return mergesArray;
+  }
+
+  private toRowData(sheetsData: string[][], merges, titles: string[]) {
+    const rowData = [];
+    merges.forEach(merge => {
+      for (let i = merge.startRowIndex; i < merge.endRowIndex; i++) {
+        const groupRowIndex = merge.startRowIndex - 1;
+        const row = {
+          groupTitle: sheetsData[groupRowIndex][0],
+          subGroups: [],
+        };
+
+        sheetsData[i].forEach((value, index) => {
+          if (!this.isInSubGroup(merge.subGroups, index)) {
+            row[titles[index]] = value;
+          }
+        });
+
+        merge.subGroups.forEach(subGroupDelimiter => {
+          const subGroup = {
+            title:
+              sheetsData[groupRowIndex][subGroupDelimiter.startColumnIndex],
+            headerTitle: titles[subGroupDelimiter.startColumnIndex],
+            values: sheetsData[i].slice(
+              subGroupDelimiter.startColumnIndex,
+              subGroupDelimiter.endColumnIndex,
+            ),
+          };
+          row.subGroups.push(subGroup);
+        });
+
+        rowData.push(row);
+      }
+    });
+    return rowData;
   }
 
   async insertRow(accessToken: string, spreadsheetId: string, row: any) {
@@ -43,226 +141,12 @@ export class SheetsService {
     return await sheets.spreadsheets.values.append(appendOptions); */
 
     const range = 'A1:Z1000';
-    return await sheets.spreadsheets.values.get({
+    const test = await this.getData(accessToken, spreadsheetId);
+    return test;
+    /* return await sheets.spreadsheets.values.get({
       spreadsheetId,
       range,
-    });
-
-    // ->
-    /* {
-      "range": "Sheet1!A1:W1000",
-      "majorDimension": "ROWS",
-      "values": [
-        [
-          "Date",
-          "Humeur",
-          "BPM",
-          "Répétitions",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "Repetitions"
-        ],
-        [
-          "Niveau 2",
-          "",
-          "",
-          "B1",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "A3"
-        ],
-        [
-          "06/01/2016",
-          "💪🏼",
-          "",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5"
-        ],
-        [
-          "08/01/2016",
-          "💪🏼",
-          "",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6"
-        ],
-        [
-          "18/01/2016",
-          "💪🏼",
-          "",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5"
-        ],
-        [
-          "20/01/2016",
-          "💪🏼",
-          "",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6"
-        ],
-        [
-          "30/12/2019",
-          "💪🏼",
-          "",
-          "7",
-          "7",
-          "7",
-          "7",
-          "7",
-          "7",
-          "7",
-          "7",
-          "7",
-          "7",
-          "7",
-          "7"
-        ],
-        [
-          "31/12/2019",
-          "💪🏼",
-          "",
-          "8",
-          "8",
-          "8",
-          "8",
-          "8",
-          "8",
-          "8",
-          "8",
-          "8",
-          "8",
-          "8",
-          "8"
-        ],
-        [
-          "10/01/2020",
-          "😅",
-          "",
-          "9",
-          "9",
-          "9",
-          "9",
-          "9",
-          "9",
-          "9",
-          "9",
-          "9",
-          "9",
-          "9",
-          "9"
-        ],
-        [
-          "22/01/2020",
-          "🥵",
-          "",
-          "10",
-          "10",
-          "10",
-          "10",
-          "10",
-          "10",
-          "10",
-          "10",
-          "10",
-          "10",
-          "10",
-          "10"
-        ],
-        [
-          "Niveau 3",
-          "",
-          "",
-          "B2",
-          "",
-          "",
-          "",
-          "",
-          "",
-          "A3"
-        ],
-        [
-          "23/01/2020",
-          "💪🏼",
-          "",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5"
-        ],
-        [],
-        [],
-        [
-          "06/01/2016",
-          "😅",
-          "",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "5",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6",
-          "6"
-        ]
-      ]
-    } */
+    }); */
   }
 
   /* async formatRow(accessToken: string, spreadsheetId: string, row: any) {
@@ -345,93 +229,6 @@ export class SheetsService {
   ]
 }*/
 
-  private rawSheetToMatrix(rawSheet: sheets_v4.Schema$Spreadsheet) {
-    if (!rawSheet.sheets[0].hasOwnProperty('data')) {
-      return [[]];
-    }
-
-    const sheet = rawSheet.sheets[0];
-    const rowData = sheet.data[0].rowData;
-
-    const headers = this.getFormattedValuesFromRow(rowData[0]);
-
-    const merges = {};
-    sheet.merges.forEach(merge => {
-      if (!merges.hasOwnProperty(merge.startRowIndex)) {
-        merges[merge.startRowIndex] = [];
-      }
-      merges[merge.startRowIndex].push(merge);
-    });
-
-    const groupsDelimiters = this.getGroupsDelimiters(merges, rowData.length);
-    return this.getGroups(groupsDelimiters, rowData, headers);
-  }
-
-  private getGroups(
-    groupsDelimiters: GroupDelimiter[],
-    rowData: sheets_v4.Schema$RowData[],
-    headers: string[],
-  ) {
-    return groupsDelimiters.map(groupDelimiter => {
-      return {
-        title: rowData[groupDelimiter.startRowIndex].values[0].formattedValue,
-        rowsData: rowData
-          .slice(groupDelimiter.startRowIndex + 1, groupDelimiter.endRowIndex)
-          .map(this.getFormattedValuesFromRow)
-          .map(row =>
-            this.setSubGroups(
-              row,
-              groupDelimiter.subGroups.map(subGroupDelimiter =>
-                this.getSubGroupsTitles(rowData, subGroupDelimiter),
-              ),
-              headers,
-            ),
-          ),
-      };
-    });
-  }
-
-  private getSubGroupsTitles(
-    rowData: sheets_v4.Schema$RowData[],
-    subGroupDelimiter: sheets_v4.Schema$GridRange,
-  ): GridRangeWithTitle {
-    return {
-      ...subGroupDelimiter,
-      title:
-        rowData[subGroupDelimiter.startRowIndex].values[
-          subGroupDelimiter.startColumnIndex
-        ].formattedValue,
-    };
-  }
-
-  private setSubGroups(
-    rowValues: string[],
-    subGroupsDelimiters: GridRangeWithTitle[],
-    headers: string[],
-  ) {
-    const subGroups = subGroupsDelimiters.map(subGroupDelimiter => {
-      return {
-        data: rowValues.slice(
-          subGroupDelimiter.startColumnIndex,
-          subGroupDelimiter.endColumnIndex,
-        ),
-        title: subGroupDelimiter.title,
-      };
-    });
-
-    const row = {
-      subGroups,
-    };
-
-    rowValues.forEach((value, index) => {
-      if (!this.isInSubGroup(subGroupsDelimiters, index)) {
-        row[headers[index]] = value;
-      }
-    });
-
-    return row;
-  }
-
   private isInSubGroup(
     subGroupsDelimiters: sheets_v4.Schema$GridRange[],
     index: number,
@@ -448,76 +245,10 @@ export class SheetsService {
     return isPresent;
   }
 
-  private getGroupsDelimiters(
-    merges: { [key: string]: sheets_v4.Schema$GridRange[] },
-    rowsLength: number,
-  ): GroupDelimiter[] {
-    const headerMerges = merges[0];
-    delete merges[0];
-
-    const mergesArray = Object.keys(merges).map(mergeKey => {
-      return merges[mergeKey];
-    }); // TODO: necessary ?
-
-    const groups = [];
-    mergesArray.forEach((merge, index) => {
-      const group = {
-        startRowIndex: merge[0].startRowIndex,
-        endRowIndex: rowsLength,
-        startColumnIndex: merge[0].startColumnIndex,
-        subGroups: merge.filter(item => item.startColumnIndex !== 0),
-      };
-
-      if (index + 1 < mergesArray.length) {
-        group.endRowIndex = mergesArray[index + 1][0].startRowIndex;
-      }
-      groups.push(group);
-    });
-    return groups;
-  }
-
-  private getFormattedValuesFromRow(row: sheets_v4.Schema$RowData) {
-    return row.values.map((cell, cellIndex) => {
-      /* if (this.isCellInMerge(merges, rowIndex, cellIndex)) {
-        const nb = cellIndex - merges[rowIndex].startColumnIndex + 1;
-        return `${
-          row.values[merges[rowIndex].startColumnIndex].formattedValue
-        } ${nb}`;
-      } */
-      return cell.formattedValue;
-    });
-  }
-
-  private matrixToRowData(matrix: string[][]): RowData[] {
-    const header = matrix.shift();
-    return matrix.map(row => {
-      const newRow = {};
-      header.forEach((cell, index) => {
-        newRow[cell] = row[index];
-      });
-      return newRow;
-    });
-  }
-
   private getSheetsAPI(accessToken: string) {
     return google.sheets({
       version: 'v4',
       auth: this.driveService.getConfigFromToken(accessToken),
     });
   }
-}
-
-export interface RowData {
-  [key: string]: string;
-}
-
-export interface GroupDelimiter {
-  startRowIndex: number;
-  endRowIndex: number;
-  startColumnIndex: number;
-  subGroups: sheets_v4.Schema$GridRange[];
-}
-
-export interface GridRangeWithTitle extends sheets_v4.Schema$GridRange {
-  title: string;
 }
